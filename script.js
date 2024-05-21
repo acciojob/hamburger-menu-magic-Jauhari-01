c9928a645ac1e5ed77a6e5f55c77f40ea9c47913
Shubham Jauhari <90859295+Jauhari-01@users.noreply.github.com>
2024-05-21 20:13:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+const hamburger = document.getElementById("ham");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", function() {
+    navLinks.classList.toggle("show");
+    hamburger.classList.toggle("open");
+});
+});
